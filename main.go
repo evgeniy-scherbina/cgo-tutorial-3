49ffc9c4d8b66362ccadb193ed9e4228766effac
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-06-10 09:52:19 -0400

    Commit

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import "C"
 import "fmt"
 
 func main() {
-	var uuid *C.uchar
-	uuid = (*C.uchar)(C.malloc(16))
-	var uuid_str *C.char
-	uuid_str = (*C.char)(C.malloc(37))
-	C.uuid_generate_random(uuid)
-	C.uuid_unparse(uuid, uuid_str)
-	fmt.Println(C.GoString(uuid_str))
+	{
+		var uuid *C.uchar
+		uuid = (*C.uchar)(C.malloc(16))
+		var uuid_str *C.char
+		uuid_str = (*C.char)(C.malloc(37))
+		C.uuid_generate_random(uuid)
+		C.uuid_unparse(uuid, uuid_str)
+		fmt.Println(C.GoString(uuid_str))
+	}
+
+	{
+		var uuid *C.uchar
+		uuid = (*C.uchar)(C.malloc(16))
+		var uuid_str *C.char
+		uuid_str = (*C.char)(C.malloc(37))
+		C.uuid_generate_random(uuid)
+		C.uuid_unparse(uuid, uuid_str)
+		fmt.Println(C.GoString(uuid_str))
+	}
 }
