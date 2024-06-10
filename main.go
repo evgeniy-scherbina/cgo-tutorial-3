c15e90d802c087ca01ba88531a4cc4960eb36294
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-06-09 20:08:36 -0400

    Commit

--- main.go
+++ main.go
@@ -4,8 +4,20 @@ package main
 import "C"
 import "fmt"
 
+//// In C:
+//// we need a uuid_t variable to initalize
+//uuid uuid_t;
+//// then we generate the random string. I use the random form
+//// but you can use other generate methods.
+//uuid_generate_random(uuid);
+//// To get a uuid string, we need to "unparse"
+//char uuid_str[37];
+//uuid_unparse_lower(uuid, uuid_str);
+//// In the uuid_str char*, there is a uuid
+
 func main() {
-	var uuid C.uuid_t
+	var uuid *C.uchar
+	uuid = (*C.uchar)(C.malloc(16))
 	var uuid_str *C.char
 	uuid_str = (*C.char)(C.malloc(37))
 	C.uuid_generate_random(uuid)
